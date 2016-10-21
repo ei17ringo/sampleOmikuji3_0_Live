39f040e05cc3867695ce3438570013c93e00e31b
ei17ringo <ei17ringo@gmail.com>
2016-10-21 12:13:34 +0800

    init commit

--- sampleOmikuji/ViewController.swift
+++ sampleOmikuji/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var omikuji = ["大吉","吉","中吉","小吉","末吉","凶","大凶"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func tapUranai(_ sender: UIButton) {
+        //どれかひとつをランダムに選ぶ
+        var r = Int(arc4random()) % omikuji.count
+        
+        //おみくじ結果のアラートを作る
+        var alertController = UIAlertController(title: "今日の運勢", message: omikuji[r], preferredStyle: .alert)
+        
+        //OKぼたん追加
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: {action in self.kekka()}))
+        
+        //アラートを表示
+        present(alertController, animated: true, completion: nil)
+        
+    }
+    
+    func kekka(){
+        //ここに画像表示の処理を追加
+    
+        print("OKがおされました")
+    
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
